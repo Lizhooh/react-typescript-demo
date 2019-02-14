b0c084f0e134e2bdf8d1e7ace5c0421f00169d43
lizhooh <960316408@qq.com>
2019-02-14 23:57:57 +0800

    update: config

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
     Route,
@@ -7,21 +7,40 @@ import {
 import RouterList from './routes';
 import Menu from './components/menu';
 
-export default () => (
-    <Router>
-        <div className="main">
-            <Menu list={RouterList as any} />
-            <div className="body">
-                <Switch>
-                    {RouterList.map((item, index) => (
-                        <Route
-                            exact={true}
-                            path={item.url}
-                            key={`r-${index}`}
-                            component={item.view} />
-                    ))}
-                </Switch>
-            </div>
-        </div>
-    </Router>
-);
+export default class extends Component<any, any> {
+    constructor(props) {
+        super(props);
+        this.state = { showload: false };
+    }
+    componentWillMount() {
+        // 初次加载时，先显示加载图标
+        this.setState({ showload: true });
+        // 通过 token 校验后再显示界面
+        setTimeout(() => {
+            this.setState({ showload: false });
+        }, 1000);
+        // 如果不通过则转跳到登录页
+    }
+    render() {
+        const { showload } = this.state;
+        return (
+            <Router>
+                <div className="main">
+                    <Menu list={RouterList as any} />
+                    <div className="body">
+                        <Switch>
+                            {RouterList.map((item, index) => (
+                                <Route
+                                    exact={true}
+                                    path={item.url}
+                                    key={`r-${index}`}
+                                    component={item.view} />
+                            ))}
+                        </Switch>
+                    </div>
+                    {showload && <div className='loading'>加载中...</div>}
+                </div>
+            </Router>
+        );
+    }
+}
